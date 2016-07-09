59c44b00efb8393f2c9420d0e1cf5617472753ff
Benjamin Feight <bjfeight@gmail.com>
2016-07-08 21:44:36 -0400

    Update route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'page/index'
+  root 'page#index'
 
-  get 'page/secret'
+  get '/secret' => 'page#secret'
 
-  resources :users
+  get '/register'
+
+    resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
